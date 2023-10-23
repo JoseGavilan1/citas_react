8377952bc98f7abc841fbec6f58d36c6fe870b0f
JoseGavilan1 <gavilanhidalgojose@gmail.com>
2023-10-23 14:14:13 -0300

    Ultimo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <>
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento pacientes {''} Con Github {''}<br/>
+            Seguimiento pacientes <br/>
             <span className="text-indigo-600">veterinaria</span>
         </h1>
     </>
